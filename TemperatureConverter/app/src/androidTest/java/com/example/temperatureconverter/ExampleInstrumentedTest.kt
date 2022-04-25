174b3d3863ee615e4c9e9a1f1897568623235e3b
vicky <v.sassu48@gmail.com>
2022-04-25 23:18:03 +0200

    add instrumentation tests

--- TemperatureConverter/app/src/androidTest/java/com/example/temperatureconverter/ExampleInstrumentedTest.kt
+++ TemperatureConverter/app/src/androidTest/java/com/example/temperatureconverter/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.example.temperatureconverter
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.containsString
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +21,30 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ConverterTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.temperatureconverter", appContext.packageName)
+    fun convert_F_to_C_round() {
+        onView(withId(R.id.fahrenheitOption)).perform(click())
+        onView(withId(R.id.temperatureInput_editText))
+            .perform(typeText("10.00"))
+            .perform(closeSoftKeyboard())
+        onView(withId(R.id.convertButton)).perform(click())
+        onView(withId(R.id.result))
+            .check(matches(withText(containsString("Result: -39,57 C°"))))
+    }
+
+    @Test
+    fun convert_F_to_C_no_round() {
+        onView(withId(R.id.fahrenheitOption)).perform(click())
+        onView(withId(R.id.temperatureInput_editText))
+            .perform(typeText("10.00"))
+            .perform(closeSoftKeyboard())
+        onView(withId(R.id.round_switch)).perform(click())
+        onView(withId(R.id.convertButton)).perform(click())
+        onView(withId(R.id.result))
+            .check(matches(withText(containsString("Result: -39.568345323741006 C°"))))
     }
 }
