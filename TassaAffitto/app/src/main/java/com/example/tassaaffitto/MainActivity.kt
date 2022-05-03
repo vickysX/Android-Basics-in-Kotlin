ed8f126e7150b68980734c476f917d428f78f76d
vicky <v.sassu48@gmail.com>
2022-05-03 10:18:29 +0200

    add link to webpage

--- TassaAffitto/app/src/main/java/com/example/tassaaffitto/MainActivity.kt
+++ TassaAffitto/app/src/main/java/com/example/tassaaffitto/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.tassaaffitto
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import com.example.tassaaffitto.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var webIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculationButton.setOnClickListener { calcolaRisultato() }
+
+        // make the string link spannable, underline it, and set it as text of its TextView
+        val stringLink = getString(R.string.link)
+        val spannableString = SpannableString(stringLink)
+        spannableString.setSpan(UnderlineSpan(), 0, spannableString.length, 0)
+        binding.linkAgenziaEntrate.text = spannableString
+
+        binding.linkAgenziaEntrate.setOnClickListener { apriSitoAgenzia() }
     }
 
     private fun calcolaRisultato() {
@@ -54,5 +67,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun calcolaTerzaRata(total: Double): Double {
         return total * 0.05
+    }
+
+    private fun apriSitoAgenzia() {
+        val queryUrl = Uri.parse("https://www.agenziaentrate.gov.it/portale/")
+        webIntent = Intent(Intent.ACTION_VIEW, queryUrl)
+        startActivity(webIntent)
     }
 }
