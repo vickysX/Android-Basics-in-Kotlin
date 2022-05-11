834076fcc90da0c8db578d4b01e64440961ca768
vicky <v.sassu48@gmail.com>
2022-05-11 23:10:37 +0200

    create Cupcake app

---
+++ Cupcake/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,75 @@
+package com.example.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
+class OrderViewModel : ViewModel() {
+
+    private var _quantity = MutableLiveData<Int>()
+    val quantity: LiveData<Int> get() = _quantity
+
+    private var _flavor = MutableLiveData<String>()
+    val flavor: LiveData<String> get() = _flavor
+
+    private var _date = MutableLiveData<String>()
+    val date: LiveData<String> get() = _date
+
+    private var _price = MutableLiveData<Double>()
+    val price: LiveData<Double> get() = _price
+
+    val dateOptions = getPickupOptions()
+
+    init {
+        resetOrder()
+    }
+
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+        updatePrice()
+    }
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+        updatePrice()
+    }
+
+    fun hasNoFlavourSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+    private fun updatePrice() {
+        var calculatePrice = (_quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == _date.value) {
+            calculatePrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatePrice
+    }
+}
