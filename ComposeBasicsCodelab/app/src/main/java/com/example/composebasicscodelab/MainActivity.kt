05b6a6bde9bafe690a58d05987c95e7d57bd1a8c
vicky <v.sassu48@gmail.com>
2022-05-12 22:46:15 +0200

    start Compose Basics codelab

---
+++ ComposeBasicsCodelab/app/src/main/java/com/example/composebasicscodelab/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.composebasicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composebasicscodelab.ui.theme.ComposeBasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember {mutableStateOf(false)}
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            OutlinedButton(onClick = {expanded.value = !expanded.value}) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+
+    }
+
+}
+
+@Composable
+private fun MyApp(names: List<String> = listOf("Android", "Compose")) {
+    Surface(color = MaterialTheme.colors.background, modifier = Modifier.padding(vertical = 4.dp)) {
+        Column {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    ComposeBasicsCodelabTheme {
+        MyApp()
+    }
+}
